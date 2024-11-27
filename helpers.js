ea4267051a661af14f0b4ea73b849bf21f2ee041
Mocsa <mocsa1974@gmail.com>
2024-11-27 11:03:29 +0100

    1

--- helpers.js
+++ helpers.js
@@ -3,9 +3,8 @@ exports.readLines = (file) => {
     return fs.readFileSync(file,'utf-8').split('\n\r')
 }
 exports.splitLine = (line,separator) => {
-    console.log(line);
     return line.split(separator);
 }
-exports.readints = (line,separator) => {
+exports.readInts = (line,separator) => {
     return splitLine(line,separator).map(m=>+m);
 }
